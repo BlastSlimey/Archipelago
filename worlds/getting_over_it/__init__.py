b10d3c9ee8d6decc3f0534e37f1ea6a74fa0ab3f
Slimey <blastslimeyt@gmail.com>
2025-01-14 00:04:06 +0100

    Added new spots, added float-only region, merged completions region into late region, increased Goal Height Reductions by 2

--- worlds/getting_over_it/__init__.py
+++ worlds/getting_over_it/__init__.py
@@ -1,4 +1,4 @@
-from typing import Mapping, Any, Dict
+from typing import Dict
 
 from BaseClasses import Item, Location, ItemClassification, Region
 from worlds.AutoWorld import WebWorld, World
@@ -27,12 +27,15 @@ class GOIWorld(World):
         "Goal Height Reduction": ItemClassification.progression,
         "Frustration": ItemClassification.filler,
     }
-    instant_spot = ["Tree", "Paddle"]
-    early_spots = ["Concrete Pipe", "Cup", "Trash", "Second Lamp", "Bathtub", "Grill", "Kids Slide"]
-    midgame_spots = ["Child", "Staircase", "Security Cam", "Toilet", "Orange Table", "Gargoyle", "Church Top", "Hedge",
-                     "Hat"]
-    late_spots = ["Snake Ride", "Bucket", "Landing Stage", "Ice Mountain", "Temple", "Antenna Top"]
-    spots_list = instant_spot + early_spots + midgame_spots + late_spots
+    instant_spots = ["Tree", "Paddle"]
+    early_spots = ["Concrete Pipe", "Barrels", "Cup", "Trash", "First Lamp", "Second Lamp", "Bathtub", "Grill",
+                   "Kids Slide"]
+    midgame_spots = ["Pliers", "Child", "Car", "Staircase", "Security Cam", "Toilet", "Orange Table", "Gargoyle",
+                     "Church Top", "Hedge", "Hat", "Anvil"]
+    late_spots = ["Telephone Booth", "Snake Ride", "Bucket", "Landing Stage", "Ice Mountain", "Shopping Cart", "Temple",
+                  "Antenna Top"]
+    float_only_spots = ["Big Balloon", "Sexy Hiking Character"]
+    spots_list = instant_spots + early_spots + midgame_spots + late_spots + float_only_spots
     completions_list = [f"Got Over It #{count}" for count in range(1, 10)]
     locations_list = spots_list + completions_list
 
@@ -51,28 +54,27 @@ class GOIWorld(World):
         region_early = Region("Early spots", self.player, self.multiworld)
         region_mid = Region("Midgame spots", self.player, self.multiworld)
         region_late = Region("Late spots", self.player, self.multiworld)
-        region_completions = Region("Completions", self.player, self.multiworld)
+        region_float = Region("Float-only spots", self.player, self.multiworld)
 
-        region_menu.connect(region_early, "Past tree",
+        region_menu.connect(region_early, "Past tree and paddle",
                             lambda state: state.has("Gravity Reduction", self.player, self.options.difficulty.value))
-        region_early.connect(region_mid, "Got on the slide",
+        region_early.connect(region_mid, "Getting on to the slide",
                              lambda state: state.has("Gravity Reduction", self.player,
                                                      self.options.difficulty.value + 1))
-        region_mid.connect(region_late, "From the anvil",
-                           lambda state: state.has("Gravity Reduction", self.player, self.options.difficulty.value + 2))
-        region_mid.connect(region_completions, "Reaching the top",
+        region_mid.connect(region_late, "Jumping from the anvil",
                            lambda state: state.has("Gravity Reduction", self.player, self.options.difficulty.value + 2)
                                          or state.has("Goal Height Reduction", self.player, 4))
+        region_late.connect(region_float, "Jumping to very high places",
+                            lambda state: state.has("Gravity Reduction", self.player, 4))
 
-        region_menu.locations.append(GOILocation(self.player, "Tree", self.location_name_to_id["Tree"], region_menu))
-        region_menu.locations.append(GOILocation(self.player, "Paddle", self.location_name_to_id["Paddle"],
-                                                 region_menu))
-        for count in range(1, self.options.required_completions.value):
+        for loc in self.instant_spots:  # always 2 locations
+            region_menu.locations.append(GOILocation(self.player, loc, self.location_name_to_id[loc], region_menu))
+        for count in range(1, self.options.required_completions.value):  # 0-9 locations
             loc_name = f"Got Over It #{count}"
-            region_completions.locations.append(GOILocation(self.player, loc_name, self.location_name_to_id[loc_name],
-                                                            region_late))
-        remaining_spots = self.early_spots + self.midgame_spots + self.late_spots
-        for _ in range(13 - self.options.required_completions.value):
+            region_late.locations.append(GOILocation(self.player, loc_name,
+                                                     self.location_name_to_id[loc_name], region_late))
+        remaining_spots = self.early_spots + self.midgame_spots + self.late_spots + self.float_only_spots
+        for _ in range(15 - self.options.required_completions.value):  # => 14-5 extra locations
             loc_name = remaining_spots.pop(self.random.randint(0, len(remaining_spots)-1))
             if loc_name in self.early_spots:
                 region_early.locations.append(GOILocation(self.player, loc_name, self.location_name_to_id[loc_name],
@@ -80,22 +82,25 @@ class GOIWorld(World):
             elif loc_name in self.midgame_spots:
                 region_mid.locations.append(GOILocation(self.player, loc_name, self.location_name_to_id[loc_name],
                                                         region_mid))
-            if loc_name in self.late_spots:
+            elif loc_name in self.late_spots:
                 region_late.locations.append(GOILocation(self.player, loc_name, self.location_name_to_id[loc_name],
                                                          region_late))
+            elif loc_name in self.float_only_spots:
+                region_float.locations.append(GOILocation(self.player, loc_name, self.location_name_to_id[loc_name],
+                                                          region_float))
 
-        goal_location = GOILocation(self.player, "Goal", None, region_completions)
+        goal_location = GOILocation(self.player, "Goal", None, region_late)
         goal_location.place_locked_item(GOIItem("Goal", ItemClassification.progression_skip_balancing,
                                                 None, self.player))
-        region_completions.locations.append(goal_location)
+        region_late.locations.append(goal_location)
         self.multiworld.completion_condition[self.player] = lambda state: state.has("Goal", self.player)
 
-        self.multiworld.regions.extend([region_menu, region_early, region_mid, region_late, region_completions])
+        self.multiworld.regions.extend([region_menu, region_early, region_mid, region_late, region_float])
 
     def create_items(self) -> None:
         self.multiworld.itempool.extend([self.create_item("Gravity Reduction") for _ in range(4)] +
                                         [self.create_item("Wind Trap") for _ in range(6)] +
-                                        [self.create_item("Goal Height Reduction") for _ in range(4)])
+                                        [self.create_item("Goal Height Reduction") for _ in range(6)])
 
 
 class GOIItem(Item):
