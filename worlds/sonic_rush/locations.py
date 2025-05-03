b4720d90b98f83f72c4f1c33b3bf9c737e19c45e
Slimey <blastslimeyt@gmail.com>
2025-05-03 14:09:11 +0200

    Changed location names and reworked location data transfer

--- worlds/sonic_rush/locations.py
+++ worlds/sonic_rush/locations.py
@@ -1,76 +1,101 @@
 from typing import List, Tuple, Dict, Optional
 
-from BaseClasses import Location, LocationProgressType, Region
+from BaseClasses import Location, LocationProgressType as LocProg, Region
+from . import data
 from .options import SonicRushOptions
 
 location_description = {}
 
-act_locations: List[str] = [f"Zone {zone} Act {act} {rank}({char})"
-                            for zone in [1, 2, 3, 4, 5, 6, 7]
-                            for act in [1, 2]
-                            for char in ["Sonic", "Blaze"]
-                            for rank in ["", "S Rank "]]
-boss_locations: List[str] = ([f"Zone {zone} Boss {rank}({char})"
-                              for zone in [1, 2, 3, 4, 5, 6, 7]
-                              for char in ["Sonic", "Blaze"]
-                              for rank in ["", "S Rank "]]
-                             + ["F-Zone (Sonic)", "F-Zone (Blaze)", "Extra Zone"])
-special_stage_locations: List[str] = [f"Special Stage Zone {zone}" for zone in [1, 2, 3, 4, 5, 6, 7]]
+act_locations: List[Tuple[str, str, LocProg]] = [
+    (f"{zone} Act {act} ({char})", f"{zone} ({char})", LocProg.DEFAULT)
+    for zone in data.zone_names_without_f_zone
+    for act in [1, 2]
+    for char in ["Sonic", "Blaze"]
+]
 
-all_locations: List[str] = act_locations + boss_locations + special_stage_locations
+act_s_rank_locations: List[Tuple[str, str, LocProg]] = [
+    (f"{zone} Act {act} S Rank ({char})", f"{zone} ({char})", LocProg.DEFAULT)
+    for zone in data.zone_names_without_f_zone
+    for act in [1, 2]
+    for char in ["Sonic", "Blaze"]
+]
 
-base_id = 20010707
+boss_locations: List[Tuple[str, str, LocProg]] = [
+    (f"{zone} Boss ({char})", f"{zone} ({char})", LocProg.PRIORITY)
+    for zone in data.zone_names_without_f_zone
+    for char in ["Sonic", "Blaze"]
+]
+
+boss_s_rank_locations: List[Tuple[str, str, LocProg]] = [
+    (f"{zone} Boss S Rank ({char})", f"{zone} ({char})", LocProg.PRIORITY)
+    for zone in data.zone_names_without_f_zone
+    for char in ["Sonic", "Blaze"]
+]
+
+f_zone_locations: List[Tuple[str, str, LocProg]] = [
+    ("F-Zone (Sonic)", "F-Zone (Sonic)", LocProg.PRIORITY),
+    ("F-Zone (Blaze)", "F-Zone (Blaze)", LocProg.PRIORITY),
+]
+
+screw_f_zone_locations: List[Tuple[str, str, LocProg]] = [
+    ("F-Zone (Sonic)", "F-Zone (Sonic)", LocProg.EXCLUDED),
+    ("F-Zone (Blaze)", "F-Zone (Blaze)", LocProg.EXCLUDED),
+]
+
+extra_zone_locations: List[Tuple[str, str, LocProg]] = [
+    ("Extra Zone", "Extra Zone", LocProg.PRIORITY),
+]
+
+special_stage_locations: List[Tuple[str, str, LocProg]] = [
+    (f"{zone} Special Stage", f"{zone} (Sonic)", LocProg.PRIORITY)
+    for zone in data.zone_names_without_f_zone
+]
+
+all_locations: List[str] = [
+    loc[0] for loc in (
+        act_locations +
+        act_s_rank_locations +
+        boss_locations +
+        boss_s_rank_locations +
+        f_zone_locations +
+        screw_f_zone_locations +
+        extra_zone_locations +
+        special_stage_locations
+    )
+]
+
 location_lookup_by_name: Dict[str, int] = {
-    name: next_id for next_id, name in enumerate(all_locations, base_id)
+    name: next_id for next_id, name in enumerate(all_locations, data.base_id)
 }
 
 
-def add_base_acts(options: SonicRushOptions) -> Dict[str, Tuple[str, LocationProgressType]]:
+def add_base_acts(options: SonicRushOptions) -> List[Tuple[str, str, LocProg]]:
 
-    return {
-        f"Zone {zone} Act {act} {rank}({char})": (f"Zone {zone} ({char})", LocationProgressType.DEFAULT)
-        for zone in [1, 2, 3, 4, 5, 6, 7]
-        for act in [1, 2]
-        for char in ["Sonic", "Blaze"]
-        for rank in (["", "S Rank "] if options.include_s_rank_checks in ["only_acts", "all"] else [""])
-    }
+    return act_locations + (
+        act_s_rank_locations if options.include_s_rank_checks in ["only_acts", "all"] else []
+    )
 
 
-def add_bosses(options: SonicRushOptions) -> Dict[str, Tuple[str, LocationProgressType]]:
+def add_bosses(options: SonicRushOptions) -> List[Tuple[str, str, LocProg]]:
 
-    return {
-        **{
-            f"Zone {zone} Boss {rank}({char})": (f"Zone {zone} ({char})", LocationProgressType.PRIORITY)
-            for zone in [1, 2, 3, 4, 5, 6, 7]
-            for char in ["Sonic", "Blaze"]
-            for rank in (["", "S Rank "] if options.include_s_rank_checks in ["only_bosses", "all"] else [""])
-        },
-        **{
-            "F-Zone (Sonic)": (
-                "F-Zone (Sonic)",
-                LocationProgressType.EXCLUDED if options.screw_f_zone else LocationProgressType.PRIORITY),
-            "F-Zone (Blaze)": (
-                "F-Zone (Blaze)",
-                LocationProgressType.EXCLUDED if options.screw_f_zone else LocationProgressType.PRIORITY),
-        },
-        **({
-            "Extra Zone": ("Extra Zone", LocationProgressType.PRIORITY)
-        } if not options.goal == "extra_zone" else {}),
-    }
+    return boss_locations + (
+        boss_s_rank_locations if options.include_s_rank_checks in ["only_bosses", "all"] else []
+    ) + (
+        screw_f_zone_locations if options.screw_f_zone else f_zone_locations
+    ) + (
+        extra_zone_locations if not options.goal == "extra_zone" else []
+    )
 
 
-def add_special_stages(options: SonicRushOptions) -> Dict[str, Tuple[str, LocationProgressType]]:
+def add_special_stages(options: SonicRushOptions) -> List[Tuple[str, str, LocProg]]:
 
-    return {
-        f"Special Stage Zone {zone}": (f"Zone {zone} (Sonic)", LocationProgressType.PRIORITY)
-        for zone in [1, 2, 3, 4, 5, 6, 7]
-    }
+    return special_stage_locations
 
 
 class SonicRushLocation(Location):
     game = "Sonic Rush"
 
     def __init__(self, player: int, name: str, address: Optional[int], region: Region,
-                 progress_type: LocationProgressType):
+                 progress_type: LocProg):
         super(SonicRushLocation, self).__init__(player, name, address, region)
         self.progress_type = progress_type
