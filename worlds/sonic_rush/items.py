74fa8bcc0c713127e08865df3ebf804592ab0277
Slimey <blastslimeyt@gmail.com>
2025-05-07 19:00:01 +0200

    Tweaked item classifications

--- worlds/sonic_rush/items.py
+++ worlds/sonic_rush/items.py
@@ -1,60 +1,68 @@
 from typing import Dict, Callable, Any, List
 
-from BaseClasses import Item, ItemClassification as IClass
+from BaseClasses import Item, ItemClassification as IClass, MultiWorld
 from . import data
 from .options import SonicRushOptions
 
 
-def always_progression(options: SonicRushOptions) -> IClass:
+def always_progression(options: SonicRushOptions, multiworld: MultiWorld) -> IClass:
     return IClass.progression
 
 
-def always_useful(options: SonicRushOptions) -> IClass:
+def always_progression_skip_balancing(options: SonicRushOptions, multiworld: MultiWorld) -> IClass:
+    return IClass.progression_skip_balancing
+
+
+def always_useful(options: SonicRushOptions, multiworld: MultiWorld) -> IClass:
     return IClass.useful
 
 
-def always_filler(options: SonicRushOptions) -> IClass:
+def always_filler(options: SonicRushOptions, multiworld: MultiWorld) -> IClass:
     return IClass.filler
 
 
-def always_trap(options: SonicRushOptions) -> IClass:
+def always_trap(options: SonicRushOptions, multiworld: MultiWorld) -> IClass:
     return IClass.trap
 
 
-zone_unlocks: Dict[str, Callable[[SonicRushOptions], IClass]] = {
+def fill_error_prevention(options: SonicRushOptions, multiworld: MultiWorld) -> IClass:
+    return IClass.useful if len(multiworld.worlds) <= 2 else IClass.progression
+
+
+zone_unlocks: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
     zone: always_progression
     for zone in data.zone_names
 }
 
-progressive_level_selects: Dict[str, Callable[[SonicRushOptions], IClass]] = {
-    "Progressive Level Select (Sonic)": always_progression,
-    "Progressive Level Select (Blaze)": always_progression,
+progressive_level_selects: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
+    "Progressive Level Select (Sonic)": always_progression_skip_balancing,
+    "Progressive Level Select (Blaze)": always_progression_skip_balancing,
 }
 
-emeralds: Dict[str, Callable[[SonicRushOptions], IClass]] = {
-    f"{color} {dim} Emerald": always_progression
+emeralds: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
+    f"{color} {dim} Emerald": always_progression_skip_balancing
     for color in data.emerald_colors
     for dim in ["Chaos", "Sol"]
 }
 
-sidekicks: Dict[str, Callable[[SonicRushOptions], IClass]] = {
+sidekicks: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
     "Tails": always_filler,
     "Cream": always_filler,
     "Kidnapping Tails": always_trap,
     "Kidnapping Cream": always_trap,
 }
 
-fillers: Dict[str, Callable[[SonicRushOptions], IClass]] = {
+fillers: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
     "Extra Life (Sonic)": always_filler,
     "Extra Life (Blaze)": always_filler,
 }
 
-traps: Dict[str, Callable[[SonicRushOptions], IClass]] = {
+traps: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
     "Halving Extra Lives (Sonic)": always_trap,
     "Halving Extra Lives (Blaze)": always_trap,
 }
 
-item_table: Dict[str, Callable[[SonicRushOptions], IClass]] = {
+item_table: Dict[str, Callable[[SonicRushOptions, MultiWorld], IClass]] = {
     **zone_unlocks,
     **progressive_level_selects,
     **emeralds,
