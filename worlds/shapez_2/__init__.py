5f715833b705c4cc5012da06ea041710c3bedc26
Slimey <blastslimeyt@gmail.com>
2025-12-30 01:15:16 +0100

    misc

--- worlds/shapez_2/__init__.py
+++ worlds/shapez_2/__init__.py
@@ -1,49 +1,161 @@
-from typing import List, Mapping, Any
+import logging
+from typing import List, Mapping, Any, TYPE_CHECKING
 
-from BaseClasses import MultiWorld, Item, Region
+from BaseClasses import MultiWorld, Item, Region, Tutorial
+from Options import Option, OptionError
 from worlds.AutoWorld import WebWorld, World
-from .data.strings import SPZ
-from .items import Shapez2Item, item_table, filler_items, delivery_unlocks
-from .locations import shapesanity_locations, location_table
-from .options import Shapez2Options
-from .regions import get_regions
+from . import items, locations, options, output
+
+if TYPE_CHECKING:
+    from .generate.shapes import Processor
+    from .data import AccessRule
 
 
 class Shapez2Web(WebWorld):
     rich_text_options_doc = True
     theme = "partyTime"
+    game_info_languages = ['en']
+    setup_en = Tutorial(
+        "Multiworld Setup Guide",
+        "A guide to playing shapez 2 with Archipelago:",
+        "English",
+        "setup_en.md",
+        "setup/en",
+        ["BlastSlimey"]
+    )
 
 
 class Shapez2World(World):
     """
     shapez 2 is an automation game about cutting, rotating, stacking, and painting shapes, that you extract from
-    randomly generated islands in an infinite space, without the need to manage your infinite resources or to pay for
-    building your factories.
+    randomly generated islands in an infinite space, without having to pay for building your factories.
+    It is the successor to shapez and contains a variety of more features like crystals, space platforms and belts,
+    trains, and an astounding new 3-dimensional gameplay environment in space.
     """
-    game = SPZ.game_name
-    options_dataclass = Shapez2Options
-    options: Shapez2Options
+    game = "shapez 2"
+    options_dataclass = options.Shapez2Options
+    options: options.Shapez2Options
     topology_present = True
     web = Shapez2Web()
-    base_id = 20250105
-    item_name_to_id = {name: id for id, name in enumerate(item_table.keys(), base_id)}
-    location_name_to_id = {name: id for id, name in enumerate(location_table.keys(), base_id)}
+    item_name_to_id = items.lookup_table()
+    location_name_to_id = locations.lookup_table()
+    ut_can_gen_without_yaml = True
 
     def __init__(self, multiworld: MultiWorld, player: int):
         super().__init__(multiworld, player)
 
-    def create_item(self, name: str) -> Item:
-        return Shapez2Item(name, item_table[name], self.item_name_to_id[name], self.player)
+        from .data.version import ap_minimum
+        from Utils import version_tuple
+        if version_tuple < ap_minimum():
+            raise Exception(f"Archipelago version too old for shapez 2 "
+                            f"(requires minimum {ap_minimum()}, found {version_tuple}")
+
+        self.seed: int = 0
+        self.to_be_filled_locations: int = 0
+        self.starting_processor: str | None = None
+        self.starting_items: list[str] = []  # Contains actual items AND event items
+        self.filler_nested: list[str | list] | None = None
+        self.milestone_shapes: list[tuple[list[str], list[str]]] = []
+        self.task_shapes: list[list[str]] = []
+        self.operator_shapes: list[str | None | int] = []
+        self.blueprint_shapes: list[str] = []
+        self.blueprint_points: list[int] = []
+        self.milestone_processors: list[list["Processor"]] = []
+        self.task_processors: list[list[Processor]] = []
+        self.operator_processors: list[list[Processor]] = []
+
+        self.ut_active: bool = False
+        self.location_id_to_alias: dict[int, str] = {}
+
+    def generate_early(self) -> None:
+
+        # Load values from UT if this is a regenerated world
+        if hasattr(self.multiworld, "re_gen_passthrough"):
+            if self.game in self.multiworld.re_gen_passthrough:
+                from .data import version
+
+                self.ut_active = True
+                re_gen_slot_data: dict[str, Any] = self.multiworld.re_gen_passthrough[self.game]
+                re_gen_options: dict[str, Any] = re_gen_slot_data["options"]
+                # Populate options from UT
+                for key, value in re_gen_options.items():
+                    opt: Option | None = getattr(self.options, key, None)
+                    if opt is not None:
+                        setattr(self.options, key, opt.from_any(value))
+                self.seed = re_gen_slot_data["seed"]
+                loaded_ut_version = re_gen_slot_data.get("ut_compatibility", (0, 99, 0))
+                if version.ut_compatibility() != loaded_ut_version:
+                    raise Exception("The slot you're trying to track is entirely incompatible with this apworld "
+                                    "version. Please update your apworld.")
+                if version.ut_accuracy() != loaded_ut_version:
+                    logging.warning("UT accuracy mismatch detected. You can continue tracking with this "
+                                    "apworld version, but tracking might not be entirely accurate.")
+
+        if not self.ut_active:
+            self.seed = self.random.getrandbits(64)
+
+        self.random.seed(self.seed)
+
+        locations.pre_generate_logic(self)
+        self.options.blueprint_shapes.verify_plando(self)
+        # TODO ensure there are at least 80 locations (using options), else OptionError
+
+    def create_item(self, name: str) -> items.Shapez2Item:
+        return items.generate_item(name, self)
 
     def get_filler_item_name(self) -> str:
-        return list(filler_items.keys())[self.random.randint(0, len(filler_items)-1)]
+        return items.generate_filler(self)
 
     def create_regions(self) -> None:
-        self.multiworld.regions.extend(get_regions(self.location_name_to_id, self.player, self.multiworld))
+        regions = locations.get_regions(self)
+        locations.connect_regions(self, regions)
+        processor_rules_dict: dict[list[str], "AccessRule"] = {}
+        locations.create_events(self, regions, processor_rules_dict)
+        locations.create_and_place_locations(self, regions, processor_rules_dict)
+        self.to_be_filled_locations = sum(
+            (0 if loc.item else 1)
+            for reg in regions.values()
+            for loc in reg.locations
+        )
+        self.multiworld.regions.extend(regions.values())
 
     def create_items(self) -> None:
-        self.multiworld.itempool.extend([self.create_item(key) for key in delivery_unlocks] +
-                                        [self.create_item(self.get_filler_item_name()) for _ in range(1)])
+        item_pool = items.get_main_item_pool(self)
+        self.starting_items = items.get_starting_items(self)
+        if len(item_pool) > self.to_be_filled_locations:
+            raise Exception(f"Player {self.player_name} has more guaranteed items ({len(item_pool)}) "
+                            f"than to-be-filled locations ({self.to_be_filled_locations})."
+                            f"Please report this to the apworld dev and provide the yaml used for generating.")
+        for _ in range(self.to_be_filled_locations-len(item_pool)):
+            item_pool.append(self.create_item(self.get_filler_item_name()))
+        self.multiworld.itempool.extend(item_pool)
+
+    def generate_output(self, output_directory: str) -> None:
+        output.write_scenario(self, output_directory)
 
     def fill_slot_data(self) -> Mapping[str, Any]:
-        return {"shapes": SPZ.shapes}
+        from .data import version
+
+        # Some options and data are included for UT
+        return {
+            "options": {
+                "goal": self.options.goal.current_key,
+                "location_adjustments": self.options.location_adjustments.value,
+                "shape_configuration": self.options.shape_configuration.current_key,
+                "shape_generation_modifiers": self.options.shape_generation_modifiers.value,
+                "shape_generation_adjustments": self.options.shape_generation_adjustments.value,
+                "blueprint_shapes": self.options.blueprint_shapes.to_slot_data(),
+                "item_pool_modifiers": self.options.item_pool_modifiers.value,
+                "start_inventory_from_pool": self.options.start_inventory_from_pool.value,
+            },
+            # Needed for UT
+            "seed": self.seed,
+            "ut_compatibility": version.ut_compatibility(),
+        }
+
+    @staticmethod
+    def interpret_slot_data(slot_data: dict[str, Any]) -> dict[str, Any]:
+        """Helper function for Universal Tracker"""
+        return slot_data
+
+    # TODO at the end: Remove unused imports
---
+++ worlds/shapez_2/generate/events/processors.py
@@ -0,0 +1,112 @@
+from typing import TYPE_CHECKING
+from BaseClasses import Region, LocationProgressType, ItemClassification
+
+if TYPE_CHECKING:
+    from ... import Shapez2World
+    from ...data import ExtendedRule, AccessRule
+
+
+def get_events(world: "Shapez2World", regions: dict[str, Region]) -> None:
+    from ...locations import Shapez2Location
+    from ...items import Shapez2Item
+    from ...data.rules import always_progression
+    from ..rules import extended_has_any, extended_has, extended_has_all
+
+    reg = regions["Events"]
+
+    if any(it in world.starting_items for it in ("Cutter", "Half Destroyer")):
+        world.starting_items.append("[PROCESSOR] Cutter")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Cutter", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has_any(world, state, "Cutter", "Half Destroyer")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Cutter", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if any(it in world.starting_items for it in ("Rotator (CW)", "Rotator (CCW)")):
+        world.starting_items.append("[PROCESSOR] Rotator")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Rotator", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has_any(world, state, "Rotator (CW)", "Rotator (CCW)")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Rotator", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if "2nd Floor" in world.starting_items and any(it in world.starting_items for it in ("Stacker", "Stacker (Bent)")):
+        world.starting_items.append("[PROCESSOR] Stacker")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Stacker", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has_any(world, state, "Stacker", "Stacker (Bent)")
+                          and extended_has(world, state, "2nd Floor")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Stacker", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if all(it in world.starting_items for it in ("Fluid Miner", "Painter", "Pump", "Pipe")):
+        world.starting_items.append("[PROCESSOR] Painter")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Painter", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has_all(world, state, "Fluid Miner", "Painter", "Pump", "Pipe")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Painter", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if all(it in world.starting_items for it in ("Fluid Miner", "Color Mixer", "Pump", "Pipe")):
+        world.starting_items.append("[PROCESSOR] Mixer")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Mixer", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has_all(world, state, "Fluid Miner", "Color Mixer", "Pump", "Pipe")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Mixer", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if "Pin Pusher" in world.starting_items:
+        world.starting_items.append("[PROCESSOR] Pin Pusher")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Pin Pusher", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has(world, state, "Pin Pusher")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Pin Pusher", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if all(it in world.starting_items for it in ("Fluid Miner", "Color Mixer", "Pump", "Pipe")):
+        world.starting_items.append("[PROCESSOR] Crystallizer")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Crystallizer", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has_all(world, state, "Fluid Miner", "Crystal Generator", "Pump", "Pipe")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Crystallizer", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
+
+    if "Swapper" in world.starting_items:
+        world.starting_items.append("[PROCESSOR] Swapper")
+    else:
+        loc = Shapez2Location(
+            world.player, "[PROCESSOR] Swapper", None, reg, LocationProgressType.PRIORITY,
+            lambda state: extended_has(world, state, "Swapper")
+        )
+        loc.place_locked_item(Shapez2Item(
+            "[PROCESSOR] Swapper", ItemClassification.progression, None, world.player
+        ))
+        reg.locations.append(loc)
