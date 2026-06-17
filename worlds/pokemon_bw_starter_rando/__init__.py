0e4dccb0141fec37125a19265f335265810892c1
BlastSlimey <89539656+BlastSlimey@users.noreply.github.com>
2026-06-18 01:35:13 +0200

    Starter Rando: main 0.3.31 compatibility + weighted options

--- worlds/pokemon_bw_starter_rando/__init__.py
+++ worlds/pokemon_bw_starter_rando/__init__.py
@@ -1,46 +1,37 @@
-# Do not change anything about the imports unless you know what you do
-from typing import TYPE_CHECKING, Any
-from types import ModuleType
-
-import orjson
-
-from .data import version
-
-if TYPE_CHECKING:
-    try:
+import random
+from typing import TYPE_CHECKING
+if __name__ == '__main__':
+    from data.api import PluginProtocol
+else:
+    from .data.api import PluginProtocol
+try:
+    if TYPE_CHECKING:
         from worlds.pokemon_bw.ndspy.rom import NintendoDSRom
         from worlds.pokemon_bw.rom import PokemonBWPatch
-    except:
-        pass
+        from worlds.pokemon_bw.items import PokemonBWItem
+        from worlds.pokemon_bw.data import SpeciesData, ExtendedRule
+        from worlds.pokemon_bw import PokemonBWWorld
+    from worlds.pokemon_bw.plugins._dev import DEV
+except ImportError:
+    DEV = False
 
 
-class Plugin:
-    name = version.name
+class Plugin(PluginProtocol):
+    name = "Pokemon BW Starters Rando Plugin"
+    domain = "starter_rando"
+    version = "1.1.0"
+    author = "BlastSlimey"
 
-    @staticmethod
-    def patch(rom: "NintendoDSRom", bw_patch_instance: "PokemonBWPatch", files_dump: dict[str, bytes | bytearray],
-              plugins: list[tuple[str, ModuleType]]):
-        from worlds.pokemon_bw.ndspy.narc import NARC
+    def patch(self):
         from worlds.pokemon_bw.data.pokemon.species import by_name
-        import random
-        from . import lz11
+        from worlds.pokemon_bw.patch import lz11
+        if DEV: return
 
-        slot_data = orjson.loads(bw_patch_instance.files.get("slot_data.json", b'{}'))
-        options: dict[str, Any] = slot_data.get("options", {})
-        plugin_options: dict[str, Any] = options.get("plugin_options", {})
-        seed = random.Random(slot_data.get("seed", random.randint(10000, 999999999)))
-        stat_tolerance = options.get("pokemon_randomization_adjustments", {}).get("Stats leniency", 10)
-        if "starter_rando" not in plugin_options:
-            rando_wild = options.get("randomize_wild_pokemon", [])
-            similar_stats = "similar base stats" in rando_wild
-            same_types = "type themed areas" in rando_wild
-            base_only = False
-        else:
-            this_options = plugin_options.get("starter_rando", {})
-            is_dict = isinstance(this_options, dict)
-            similar_stats = is_dict and this_options.get("similar_stats", False)
-            same_types = is_dict and this_options.get("same_types", False)
-            base_only = is_dict and this_options.get("base_only", False)
+        stat_tolerance = self.general_options["pokemon_randomization_adjustments"]["Stats leniency"]
+        rando_wild = self.general_options["randomize_wild_pokemon"]
+        similar_stats = self.get_option("similar_stats", "similar base stats" in rando_wild, int)
+        same_types = self.get_option("same_types", "type themed areas" in rando_wild, int)
+        base_only = self.get_option("base_only", False, int)
         stats_total = lambda data: (
             data.base_hp + data.base_attack + data.base_defense +
             data.base_sp_attack + data.base_sp_defense + data.base_speed
@@ -74,39 +65,50 @@ class Plugin:
             if base_only and dat.evolution_stage != 1:
                 continue
             possible_water.append(dat)
-        snivy = seed.choice(possible_grass)
-        tepig = seed.choice(possible_fire)
-        oshawott = seed.choice(possible_water)
+        snivy = self.random.choice(possible_grass)
+        tepig = self.random.choice(possible_fire)
+        oshawott = self.random.choice(possible_water)
 
-        narc = NARC(rom.getFileByName("a/0/5/7"))
-        script = bytearray(narc.files[782])
-        script = script.replace(b'\x28\x00\x21\x80\xEF\x01',
-                                b'\x28\x00\x21\x80' + snivy.dex_number.to_bytes(2, "little"))
-        script = script.replace(b'\x57\x00\x01\xEF\x01', b'\x57\x00\x01' + snivy.dex_number.to_bytes(2, "little"))
-        script = script.replace(b'\x28\x00\x21\x80\xEF\x01',
-                                b'\x28\x00\x21\x80' + tepig.dex_number.to_bytes(2, "little"))
-        script = script.replace(b'\x57\x00\x01\xEF\x01', b'\x57\x00\x01' + tepig.dex_number.to_bytes(2, "little"))
-        script = script.replace(b'\x28\x00\x21\x80\xEF\x01',
-                                b'\x28\x00\x21\x80' + oshawott.dex_number.to_bytes(2, "little"))
-        script = script.replace(b'\x57\x00\x01\xEF\x01', b'\x57\x00\x01' + oshawott.dex_number.to_bytes(2, "little"))
-        narc.files[782] = bytes(script)
-        files_dump[f"a057/782"] = narc.files[782]
-        rom.setFileByName("a/0/5/7", narc.save())
+        script = self.get_from_narc("a/0/5/7", 782)
 
-        starter_narc = NARC(rom.getFileByName("a/2/0/5"))
-        sprites_narc = NARC(rom.getFileByName("a/0/0/4"))
-        starter_narc.files[0] = sprites_narc.files[snivy.dex_number * 20 + 18]
-        starter_narc.files[2] = sprites_narc.files[tepig.dex_number * 20 + 18]
-        starter_narc.files[4] = sprites_narc.files[oshawott.dex_number * 20 + 18]
-        starter_narc.files[12] = lz11.decomp(sprites_narc.files[snivy.dex_number * 20], 0)
-        starter_narc.files[13] = lz11.decomp(sprites_narc.files[tepig.dex_number * 20], 0)
-        starter_narc.files[14] = lz11.decomp(sprites_narc.files[oshawott.dex_number * 20], 0)
-        files_dump[f"a205/0"] = starter_narc.files[0]
-        files_dump[f"a205/2"] = starter_narc.files[2]
-        files_dump[f"a205/4"] = starter_narc.files[4]
-        files_dump[f"a205/12"] = starter_narc.files[12]
-        files_dump[f"a205/13"] = starter_narc.files[13]
-        files_dump[f"a205/14"] = starter_narc.files[14]
-        rom.setFileByName("a/2/0/5", starter_narc.save())
+        index = script.find(b'\x28\x00\x21\x80\xEF\x01')
+        while index != -1:
+            script[index:index+6] = b'\x28\x00\x21\x80' + snivy.dex_number.to_bytes(2, "little")
+            index = script.find(b'\x28\x00\x21\x80\xEF\x01', index+1)
+        index = script.find(b'\x57\x00\x01\xEF\x01')
+        while index != -1:
+            script[index:index+5] = b'\x57\x00\x01' + snivy.dex_number.to_bytes(2, "little")
+            index = script.find(b'\x57\x00\x01\xEF\x01', index+1)
+
+        index = script.find(b'\x28\x00\x21\x80\xf2\x01')
+        while index != -1:
+            script[index:index+6] = b'\x28\x00\x21\x80' + tepig.dex_number.to_bytes(2, "little")
+            index = script.find(b'\x28\x00\x21\x80\xf2\x01', index+1)
+        index = script.find(b'\x57\x00\x01\xf2\x01')
+        while index != -1:
+            script[index:index+5] = b'\x57\x00\x01' + tepig.dex_number.to_bytes(2, "little")
+            index = script.find(b'\x57\x00\x01\xf2\x01', index+1)
+
+        index = script.find(b'\x28\x00\x21\x80\xf5\x01')
+        while index != -1:
+            script[index:index+6] = b'\x28\x00\x21\x80' + oshawott.dex_number.to_bytes(2, "little")
+            index = script.find(b'\x28\x00\x21\x80\xf5\x01', index+1)
+        index = script.find(b'\x57\x00\x01\xf5\x01')
+        while index != -1:
+            script[index:index+5] = b'\x57\x00\x01' + oshawott.dex_number.to_bytes(2, "little")
+            index = script.find(b'\x57\x00\x01\xf5\x01', index+1)
+
+        self.get_from_narc("a/2/0/5", 0)[:] = self.get_from_narc("a/0/0/4", snivy.dex_number * 20 + 18)
+        self.get_from_narc("a/2/0/5", 2)[:] = self.get_from_narc("a/0/0/4", tepig.dex_number * 20 + 18)
+        self.get_from_narc("a/2/0/5", 4)[:] = self.get_from_narc("a/0/0/4", oshawott.dex_number * 20 + 18)
+        self.get_from_narc("a/2/0/5", 12)[:] = lz11.decomp(self.get_from_narc("a/0/0/4", snivy.dex_number * 20), 0)
+        self.get_from_narc("a/2/0/5", 13)[:] = lz11.decomp(self.get_from_narc("a/0/0/4", tepig.dex_number * 20), 0)
+        self.get_from_narc("a/2/0/5", 14)[:] = lz11.decomp(self.get_from_narc("a/0/0/4", oshawott.dex_number * 20), 0)
 
         # UPR also changes the IDs used in the Dreamyard script, the text file of the player room, and the cry overlay
+
+
+if __name__ == '__main__':
+    from data.build import build
+
+    build(Plugin.name, Plugin.version, Plugin.author)
